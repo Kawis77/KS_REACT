8d7bd7f689b50885925588e004c4c29e3131c674
Kawis77 <krzysztofsiwak77@gmail.com>
2023-08-09 23:47:49 +0200

    create new view for one speicfic document

--- src/Document/pages/DocumentList.js
+++ src/Document/pages/DocumentList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import DocumentNavigationBar from '../components/DocumentNavigationBar';
 import Sidebar from '../components/Sidebar';
 import { Table } from 'antd';
@@ -6,13 +7,15 @@ import './../../../src/Document/styles/DocumentList.css';
 import { FileOutlined } from '@ant-design/icons';
 
 function DocumentList() {
+  const navigate = useNavigate();
+
   const [data, setData] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:8080/api/document/read/all')
       .then(response => response.json())
-      .then(data => setData(data));
-      console.log();
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
   }, []);
 
   const columns = [
@@ -21,7 +24,7 @@ function DocumentList() {
       dataIndex: '',
       key: '',
       render: () => (
-          <span style={{ fontSize: '24px' }}>
+        <span style={{ fontSize: '24px' }}>
           <FileOutlined />
         </span>
       ),
@@ -29,45 +32,52 @@ function DocumentList() {
     {
       title: 'Nazwa',
       dataIndex: 'title',
-      key: 'title'
+      key: 'title',
     },
     {
       title: 'Wlasciciel',
       dataIndex: 'owner',
       key: 'owner',
-      render: owner => owner.name + " "+ owner.surname
+      render: owner => owner.name + ' ' + owner.surname,
     },
     {
       title: 'Kategoria',
       dataIndex: 'categoryEntity',
       key: 'category',
-      render: categoryEntity => categoryEntity.name
+      render: categoryEntity => categoryEntity.name,
     },
     {
       title: 'Lokacja',
       dataIndex: 'location',
       key: 'location',
-      render: categoryEntity => categoryEntity.name
+      render: categoryEntity => categoryEntity.name,
     },
     {
       title: 'Wersja',
       dataIndex: 'version',
-      key: 'version'
+      key: 'version',
     },
     {
       title: 'Data dodania',
       dataIndex: 'createDate',
-      key: 'createDate'
-    }
+      key: 'createDate',
+    },
   ];
 
   return (
     <div>
-      {/* Załóżmy, że DocumentNavigationBar i Sidebar to osobne komponenty */}
       <DocumentNavigationBar />
       <Sidebar />
       <div className="table-container">
-        <Table dataSource={data} columns={columns} />
+        <Table
+          dataSource={data}
+          columns={columns}
+          onRow={(record) => ({
+            onDoubleClick: () => {
+              navigate(`/show-document/${record.id}`); // Zmień ścieżkę na '/document/:id'
+            },
+          })}
+        />
       </div>
     </div>
   );

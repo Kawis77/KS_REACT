90e858f8efb4434f112224704dd8bfa916f53c9a
Kawis77 <krzysztofsiwak77@gmail.com>
2023-10-02 21:42:59 +0200

    style for edit external document

--- src/Document/components/ExternalDocumentForm.js
+++ src/Document/components/ExternalDocumentForm.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState } from 'react';
 import { Form, Button, Row, Col, Tabs, Tab } from 'react-bootstrap';
-import UserPicker from '../../Application/components/dialogs/UserPicker'; 
+import UserPicker from '../../Application/components/dialogs/UserPicker';
 import axios from 'axios';
 import LocationPicker from '../../Application/components/dialogs/LocationPicker';
 import CategoryPicker from '../../Application/components/dialogs/CategoryPicker';
+import './../../../src/Document/styles/ExternalDocumentForm.css';
 
 const ExternalDocumentForm = () => {
   const [activeTab, setActiveTab] = useState('tab1');
@@ -12,6 +13,7 @@ const ExternalDocumentForm = () => {
   const [owner, setOwner] = useState(null);
   const [location, setLocation] = useState(null);
   const [category, setCategory] = useState(null);
+  const [path , setPath] = useState(null);
 
   const handleUserSelected = (user) => {
     console.log(user);
@@ -24,7 +26,7 @@ const ExternalDocumentForm = () => {
   };
 
   const handleCategorySelected = (category) => {
-     console.log(category);
+    console.log(category);
     setCategory(category);
   };
 
@@ -42,15 +44,10 @@ const ExternalDocumentForm = () => {
         version: '1',
         publicationNote: formData.get('publicationNote'),
         content: editorData
-        
+
       };
       setSummaryData(newSummaryData);
     }
-  };
-
-  const onEditorChange = (event, editor) => {
-    const data = editor.getData();
-    setEditorData(data);
   };
 
   const handleSave = async (event) => {
@@ -71,35 +68,46 @@ const ExternalDocumentForm = () => {
     };
 
     try {
-        const response = await axios.post('http://localhost:8080/api/document/external/create', documentData, {
+      const response = await axios.post('http://localhost:8080/api/document/external/create', documentData, {
+        headers: {
+          'Content-Type': 'multipart/form-data', // Ustaw odpowiedni nagłówek dla przesyłania plików
+        },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+  const handleFileChange = async (event) => {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      // Odczytaj zawartość pliku jako URL
+      const fileURL = URL.createObjectURL(selectedFile);
+      // Prześlij plik na serwer
+      const formData = new FormData();
+      formData.append('file', selectedFile);
+
+      try {
+        const response = await axios.post('http://localhost:8080/api/file/save', formData, {
           headers: {
-            'Content-Type': 'multipart/form-data', // Ustaw odpowiedni nagłówek dla przesyłania plików
+            'Content-Type': 'multipart/form-data',
           },
         });
-      } catch (error) {
-        console.error(error);
-      }
-    };
+        const uploadedFilePath = response.data;
 
-  const summaryTabContent = (documentData) => (
-    <div>
-      <Row>
-        <Col>
-          <p><strong>Tytuł:</strong> {documentData.title}</p>
-          <p><strong>Wlasciciel:</strong> {documentData.owner}</p>
-          <p><strong>Data wydania:</strong> {documentData.createDate}</p>
-          <p><strong>Lokacja:</strong> {documentData.location}</p>
-          <p><strong>Kategoria:</strong> {documentData.category}</p>
-          <p><strong>Wersja:</strong> {documentData.version}</p>
-          <p><strong>Notka publikacji:</strong> {documentData.publicationNote}</p>
-        </Col>
-      </Row>
-    </div>
-  );
+       setPath(uploadedFilePath);
+        const iframe = document.getElementById('file-content').querySelector('iframe');
+        iframe.src = iframe.src;
+      } catch (error) {
+        console.error('Błąd przesyłania pliku na serwer', error);
+      }
+    }
+  };
 
   return (
     <div>
-       <Form id='external-document-form' onSubmit={handleSave}>
+      <Form id='external-document-form' onSubmit={handleSave}>
         <Tabs activeKey={activeTab} onSelect={handleSelect} variant='pils'>
           <Tab eventKey="tab1" title="Dane">
             <Row>
@@ -110,10 +118,10 @@ const ExternalDocumentForm = () => {
                 </Form.Group>
               </Col>
               <Col>
-              <Form.Group controlId="formOwner">
-                <Form.Label>Wlasciciel</Form.Label>
-                <UserPicker onUserSelected={handleUserSelected} />
-              </Form.Group>
+                <Form.Group controlId="formOwner">
+                  <Form.Label>Wlasciciel</Form.Label>
+                  <UserPicker onUserSelected={handleUserSelected} />
+                </Form.Group>
               </Col>
             </Row>
 
@@ -127,7 +135,7 @@ const ExternalDocumentForm = () => {
               <Col>
                 <Form.Group controlId="formLocation">
                   <Form.Label>Lokacja</Form.Label>
-                 <LocationPicker onLocationSelected={handleLocationSelected} />
+                  <LocationPicker onLocationSelected={handleLocationSelected} />
                 </Form.Group>
               </Col>
             </Row>
@@ -140,52 +148,63 @@ const ExternalDocumentForm = () => {
                 </Form.Group>
               </Col>
               <Col>
-        <Form.Group controlId="formVersion">
-          <Form.Label>Wersja</Form.Label>
-          <Form.Control
-            name="version"
-            type="text"
-            placeholder="Wersja dokumentu"
-            value="1" // Ustawia domyślną wartość 1
-            readOnly 
-          />
-        </Form.Group>
-      </Col>
+                <Form.Group controlId="formVersion">
+                  <Form.Label>Wersja</Form.Label>
+                  <Form.Control
+                    name="version"
+                    type="text"
+                    placeholder="Wersja dokumentu"
+                    value="1" // Ustawia domyślną wartość 1
+                    readOnly
+                  />
+                </Form.Group>
+              </Col>
             </Row>
             <Form.Group controlId="formPublicationNote">
               <Form.Label>Notka publikacji</Form.Label>
               <Form.Control name='publicationNote' as="textarea" rows={3} />
             </Form.Group>
           </Tab>
-<Tab eventKey="tab2" title="Kontent">
- <Row>
-   <Col>
-    <Form.Group controlId="formFile">
-    <Form.Label>Dołącz plik</Form.Label>
-      <Form.Control name="documentFile" type="file" />
-    </Form.Group>
-  </Col>
-</Row>
- </Tab>
+          <Tab eventKey="tab2" title="Kontent">
+            <div className='container-fluid'>
+              <Row>
+                <div id='file-upload' class='col-6'>
+                  <Form.Group controlId="formFile">
+                    <Form.Label>Dołącz plik</Form.Label>
+                    <Form.Control name="documentFile" type="file" onChange={handleFileChange} />
+                  </Form.Group>
+                </div>
+                <div id='file-content' class='col-6'>
+                  <iframe src={`http://localhost:8080/api/document/show/one/${path}`} width="100%" height="600" title="Dokument" />
+                </div>
+              </Row>
+            </div>
+          </Tab>
           <Tab eventKey="tab3" title="Dostep">
           </Tab>
-<Tab eventKey="tab4" title="Podsumowanie">
-  <div>
-    <Row>
-      <Col>
-        <p><strong>Tytuł:</strong> {summaryData.title}</p>
-        <p><strong>Wlasciciel:</strong> {summaryData.owner}</p>
-        <p><strong>Data wydania:</strong> {summaryData.createDate}</p>
-        <p><strong>Lokacja:</strong> {summaryData.location}</p>
-        <p><strong>Kategoria:</strong> {summaryData.category}</p>
-        <p><strong>Wersja:</strong> {summaryData.version}</p>
-        <p><strong>Notka publikacji:</strong> {summaryData.publicationNote}</p>
-      </Col>
-    </Row>
-    <Button className='save-button' variant="primary" type="submit">
-      Zapisz
-    </Button>
-  </div>
+          <Tab eventKey="tab4" title="Podsumowanie">
+
+<div class="container-fluid">
+  <Row>
+    <div className='col-6' id='summary-view'>
+      <p><strong>Tytuł:</strong> {summaryData.title}</p>
+      <p><strong>Właściciel:</strong> {summaryData.owner}</p>
+      <p><strong>Data wydania:</strong> {summaryData.createDate}</p>
+      <p><strong>Lokacja:</strong> {summaryData.location}</p>
+      <p><strong>Kategoria:</strong> {summaryData.category}</p>
+      <p><strong>Wersja:</strong> {summaryData.version}</p>
+      <p><strong>Notka publikacji:</strong> {summaryData.publicationNote}</p>
+      <Button className="save-button" variant="primary" type="submit" >
+        Zapisz
+      </Button>
+      <div id='file-view'>
+      </div>
+    </div>
+    <div id='file-content' class='col-6'>
+      <iframe src={`http://localhost:8080/api/document/show/one/${path}`} width="100%" height="600" title="Dokument" />
+    </div>
+  </Row>
+</div>
 </Tab>
         </Tabs>
       </Form>
